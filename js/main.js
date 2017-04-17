dae3da7ca50ac57f0b068a6ff657ca1d04d09fdc
Zachary Durland <zacharydurland@gmail.com>
2017-04-18 07:29:11 +0900

    add status class to channels and channel filter
    dynamically add classes to each channel that allow them to be filtered
    by the online offline filter at top of page.

--- js/main.js
+++ js/main.js
@@ -60,11 +60,11 @@ $(document).ready(function(){
                     if(channels[channel][0]===null){
                         return "Offline";
                     } else {
-                        return "Streaming " + channels[channel][1];
+                        return "Streaming" + " " +channels[channel][1];
                     }
                 };
                 
-                var channelRow = '<div id="' + data.display_name +'" class="row"><div class="container-fluid channel"><div class="panel-body"><div class="channel-logo col-md-4"><a href ="' + data.url + '"><img src="' + data.logo + '"></a></div>' + '<div class="channel-id col-md-4"><a href="' + data.url + '">' + data.display_name + '</a></div><div class="stream-status col-md-4">' + statusPhrase() + '</div></div></div></div>';
+                var channelRow = '<div id="' + data.display_name +'" class="row channel ' + statusPhrase() +'"><div class="container-fluid"><div class="panel-body"><div class="channel-logo col-md-4"><a href ="' + data.url + '"><img src="' + data.logo + '"></a></div>' + '<div class="channel-id col-md-4"><a href="' + data.url + '">' + data.display_name + '</a></div><div class="stream-status col-md-4">' + statusPhrase() + '</div></div></div></div>';
                 
                 $("body").append(channelRow);
                    
@@ -73,6 +73,14 @@ $(document).ready(function(){
         }}); 
          
     });}, 1000);
+    
+    $('#now-streaming-btn').on('click',function(){
+        $("div.Offline").hide();
+    });
+    
+    $('#all-channels-btn').on('click',function(){
+        $("div.Offline").show();
+    });
         
 
 }); // end main function
