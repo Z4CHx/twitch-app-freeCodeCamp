290a2163519ece4fa19469354e687194618f9745
Zachary Durland <zacharydurland@gmail.com>
2017-04-17 21:03:54 +0900

    refactor to improve stream status display
    try to preload status into an object to pull from when loading each
    channel rather than all at once.
    
    Add a timeout to pad loading.

--- js/main.js
+++ js/main.js
@@ -1,59 +1,82 @@
 $(document).ready(function(){
     
-    var channels = ["dootbeep","bort_hammer","freecodecamp","ESL_SC2", "OgamingSC2", "cretetion","habathcx", "RobotCaleb", "noobs2ninjas"];
+    var channelList = ["dootbeep","bort_hammer","freecodecamp","ESL_SC2", "OgamingSC2", "cretetion","habathcx", "RobotCaleb", "noobs2ninjas","brunofin","comster404","yaraskygaming"];
+    
+    var channels = {
+        
+        dootbeep : ["",""],
+        bort_hammer : ["",""],
+        freecodecamp : ["",""],
+        ESL_SC2 : ["",""],
+        OgamingSC2 : ["",""],
+        cretetion : ["",""],
+        habathcx : ["",""],
+        RobotCaleb : ["",""],
+        noobs2ninjas : ["",""],
+        brunofin : ["",""],
+        comster404 : ["",""],
+        yaraskygaming :  ["",""]
+    };
+    
+
     
     
-     
-  var displayChannel = function(channel){
-    //display channel info
-    $.ajax({ 
-    type:"GET",
-    url: "https://api.twitch.tv/kraken/channels/" + channel,
-    headers:{
-        "Client-ID": "ljvzlno3ci0iq7l7dl0hxww9dx0j0b",
-    },
-    success: function(data){
-
-        var userInfo = '<div="row"><div class="container-fluid channel"><div class="panel-body"><div class="channel-logo col-md-4"><a href ="' + data.url + '"><img src="' + data.logo + '"></a></div>' + '<div class="channel-id col-md-4"><a href="' + data.url + '">' + data.display_name + '</a></div><div class="stream-status col-md-4"></div></div></div></div>';
-
-        console.log(data);
-        console.log(data.display_name);
-        $("body").append(userInfo); 
-    }
+    channelList.forEach(function(channel){
+        
+        $.ajax({ 
+        type:"GET",
+        url: "https://api.twitch.tv/kraken/streams/" + channel,
+        headers:{
+            "Client-ID": "ljvzlno3ci0iq7l7dl0hxww9dx0j0b",
+        },
+        success: function(data){
+            if(channels.hasOwnProperty(channel)) {
+                channels[channel][0] = data.stream;
+                console.log(channels[channel][0]);
+            }
+        }});
     });
+    
+    
+    
+    
+    
+    setTimeout(function(){
 
-    //display status  
-    $.ajax({ 
-    type:"GET",
-    url: "https://api.twitch.tv/kraken/streams/" + channel,
-    headers:{
-        "Client-ID": "ljvzlno3ci0iq7l7dl0hxww9dx0j0b",
-    },
-    success: function(data){
+    channelList.forEach(function(channel){
+        
+        $.ajax({ 
+        type:"GET",
+        url: "https://api.twitch.tv/kraken/channels/" + channel,
+        headers:{
+            "Client-ID": "ljvzlno3ci0iq7l7dl0hxww9dx0j0b",
+        },
+        success: function(data){
+            if(channels.hasOwnProperty(channel)) {
+                channels[channel][1] = data.game;
+                console.log(channels[channel][1]);
+                
+                var statusPhrase = function(){
+                    if(channels[channel][0]===null){
+                        return "Offline";
+                    } else {
+                        return "Streaming " + channels[channel][1];
+                    }
+                };
+                
+                var channelRow = '<div id="' + data.display_name +'" class="row"><div class="container-fluid channel"><div class="panel-body"><div class="channel-logo col-md-4"><a href ="' + data.url + '"><img src="' + data.logo + '"></a></div>' + '<div class="channel-id col-md-4"><a href="' + data.url + '">' + data.display_name + '</a></div><div class="stream-status col-md-4">' + statusPhrase() + '</div></div></div></div>';
+                
+                $("body").append(channelRow);
+                   
+            }
 
-        console.log(data);
-        if (data.stream !== null){
-            $("div.stream-status").text("Streaming");
-        } else {
-            $("div.stream-status").text("Not Streaming"); 
-        }
+        }}); 
+         
+    });}, 1000);
+        
 
-    }
-    });
+}); // end main function
 
-  };
-    
-    
-  channels.forEach(displayChannel);  
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
-});
+
+
