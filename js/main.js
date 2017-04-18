b2056af518f455b68c6a1d7ce233f047279d3ead
Zachary Durland <zacharydurland@gmail.com>
2017-04-18 11:16:18 +0900

    add comments, a credit, and clean up js

--- js/main.js
+++ js/main.js
@@ -18,9 +18,8 @@ $(document).ready(function(){
         yaraskygaming :  ["",""]
     };
     
-
     
-    
+    // get the stream status and write to channels
     channelList.forEach(function(channel){
         
         $.ajax({ 
@@ -32,17 +31,15 @@ $(document).ready(function(){
         success: function(data){
             if(channels.hasOwnProperty(channel)) {
                 channels[channel][0] = data.stream;
-                console.log(channels[channel][0]);
             }
         }});
     });
+      
     
-    
-    
-    
-    
+    // set a timeout to alleviate call timing errors, will refactor with promises
     setTimeout(function(){
-
+        
+    // look at each channel in the channelList and do the following...
     channelList.forEach(function(channel){
         
         $.ajax({ 
@@ -59,45 +56,43 @@ $(document).ready(function(){
                 
                 $("div#channels").append(channelRow);
                 
-            }
-        },
-            
+            } // end channelRow
+        }, // end status code
+        
+        // build valid channels (excludes channels not-found)
         success: function(data){
+            
+            // write null or streaming plus game title accordingly
             if(channels.hasOwnProperty(channel)) {
                 channels[channel][1] = data.game;
-                console.log(channels[channel][1]);
-                
                 var statusPhrase = function(){
                     if(channels[channel][0]===null){
                         return "Offline";
                     } else {
                         return "Streaming" + " " + channels[channel][1];
                     }
-                };
+                }; // end statusPhrase
                 
+                // buildout and display based data of both API calls
                 var channelRow = '<div id="' + data.display_name +'" class="row channel ' + statusPhrase() +'"><div class="container-fluid"><div class="panel-body"><div class="channel-logo col-md-4"><a href ="' + data.url + '"><img src="' + data.logo + '"></a></div>' + '<div class="channel-id col-md-4"><a href="' + data.url + '">' + data.display_name + '</a></div><div class="stream-status col-md-4">' + statusPhrase() + '</div></div></div></div>';
                 
                 $("div#channels").append(channelRow);
                    
-            }
+            } // end check if channels object has my channels
 
         }, //end success function
         
-        error:  function (request, status, error) {}
-            
-        }); 
+        // see status code above
+        error:  function (request, status, error){}
+        
+        }); // end call for channel info ( not stream status )
          
-    });}, 250);
+    }); // end forEach that cycles through channelList item
+    
+    }, 250); // end setTimeout
     
     
-    
-    
-    
-    
-    
-    
-    
-    
+    // listen for user click to sort channels based on a class that was appended when each channel was created and displayed.
     $('#now-streaming-btn').on('click',function(){
         $("div.Offline").hide();
         $(this).addClass("active");
